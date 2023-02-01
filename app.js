e40badd76ba97b1980f4bd40e0d882cf873d234a
91song4 <91song4@daum.net>
2023-02-02 02:48:46 +0900

    기본설정중

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`http://127.0.0.1:${process.env.PORT}`);
+});
