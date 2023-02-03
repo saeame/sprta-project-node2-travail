e8ad6b58a09ff20059ce940ec78b6f1be7c7daa5
91song4@daum.net <91song4@daum.net>
2023-02-03 12:45:57 +0900

    feat: 에러핸들링 추가

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ require("dotenv").config();
 app.use('/users', userRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
+app.use((err, req, res, next) => {
+  console.log({ err });
+  res.status(err.code).send({ errorMessage: err.message });
+})
+
 app.listen(process.env.PORT, () => {
-    console.log(`http://127.0.0.1:${process.env.PORT}`);
+  console.log(`http://127.0.0.1:${process.env.PORT}`);
 });
